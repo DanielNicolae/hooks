d7c87de04847523b1effb859b56d842f3dd1109a
DanielNicolae <deadlypoet@protonmail.com>
2021-12-09 18:25:23 +0200

    I added the state count, plus a function incrementCount, that increments count with 1. I displayed the state of count and a button to increment the state.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
-
+      <div>{count}</div>
+      <button onClick={incrementCount}>Increment</button>
     </div>
   );
 }
