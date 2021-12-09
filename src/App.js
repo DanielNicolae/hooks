034c087322a7b58b1806910c0a0896cb16a17601
DanielNicolae <deadlypoet@protonmail.com>
2021-12-09 18:46:23 +0200

    I added useEffect that changes the color of the count from black to red and from red to black, when you click on the text.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -7,9 +7,29 @@ function App() {
     setCount(count + 1);
   };
 
+  const [color, setColor] = useState("black");
+
+  useEffect(() => {
+    const changeColorOnClick = () => {
+      if (color === "black") {
+        setColor("red");
+      } else {
+        setColor("black");
+      }
+    };
+    document.addEventListener("click", changeColorOnClick);
+    return () => {
+      document.removeEventListener("click", changeColorOnClick);
+    };
+  }, [color]);
+
   return (
     <div className="App">
-      <div>{count}</div>
+      <div id="count"
+        style={{ color: color }}
+      >
+        {count}
+      </div>
       <button onClick={incrementCount}>Increment</button>
     </div>
   );
